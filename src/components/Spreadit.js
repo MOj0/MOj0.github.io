2ddb1717dc5a4d87c8809349bf6526315b0d03a9
unknown <matija.ojo@gmail.com>
2022-06-18 17:21:39 +0200

    Minor fixes

--- src/components/Spreadit.js
+++ src/components/Spreadit.js
@@ -4,19 +4,19 @@ const Spreadit = () => {
             <p>
                 This is a project made for the Information Systems class.
                 Unfortunately the repository is private and I'm not the owner, so
-                you can't see the code.
+                the code is not available.
                 <br/>
                 We created a webapp using <code>C#</code> and <code>.NET 6</code>. I learned
                 how the MVC (Model-View-Controller) works, how to set up a database in such
                 environment and connect to it through the backend. I also learned how to properly
-                store information about users (mainly passwords), authenticate and authorize them.
+                store information about users (mainly passwords), authenticate and authorize them, and implement
+                CRUD functionality..
+                The app was deployed using Microsoft Azure and is available <a target="_blank" href="http://spre4dit.azurewebsites.net/">here</a>.
                 <br/>
                 Lastly, we created an Android app which uses the same backend API, so functionality stays the same,
                 only the view changes.
                 <br/>
                 We were both very happy with the result, since we created a fully functional web (and mobile) application.
-                <br/>
-                Link to the webapp <a target="_blank" href="http://spre4dit.azurewebsites.net/">here</a>.
             </p>
         </div>
     )
--- src/components/StickmanFight.js
+++ src/components/StickmanFight.js
@@ -3,11 +3,12 @@ const StickmanFight = () => {
         <div>
             <p>
                 This project was made for the Computer Graphics class.
+                You can try out the game <a target="_blank" href="https://intense-lowlands-40528.herokuapp.com/">here</a>.
                 <br />
                 This was a big project, since we had to do everything from scratch, but
                 learned a lot in doing so. I was working on the 3D assets (models, animations in Blender),
                 making a system to import those files, and everything from the camera, player movement,
-                game logic, and shaders. My colleague was working on the networking part of the project.
+                game logic, skeletal animations, and shaders. My colleague was working on the networking part of the project.
                 <br />
                 I believe this is the largest (and hardest) project me and my colleague have ever made,
                 but it was really exciting and enlightening. Beside the technical part, another important
