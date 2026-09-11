f7c164e3495fab5e2cb374b9c4aad92734026dab
MOj0 <matija.ojo@gmail.com>
2026-09-12 00:13:38 +0200

    Updates

--- common.ps1
+++ common.ps1
@@ -6,6 +6,10 @@ if (-not (Test-Path variable:WinVer)) {
 $winutil_config_name = "win" + $WinVer + "util_config.json!"
 $ooshutup_config_name = "ooshutup" + $WinVer + ".cfg"
 
+function Print-Stuff() {
+	Write-Host $winutil_config_name
+}
+
 
 function Download-File($filename) {
 	$ConfigUrl = "https://moj0.github.io/$filename"
@@ -176,17 +180,18 @@ function Add-Godot-Exe-Env-Variable {
 }
 
 
-Win-Util
-Execute-OOShutUp10
-Refresh-Path
-Install-Scoop
-Install-Tools
-Configure-Clink
-Configure-Registry
-Add-PowerToys-Keybindings
-Install-DMZ-White
-Prompt-Install-FilePilot
-Add-Godot-Exe-Env-Variable
+# Win-Util
+# Execute-OOShutUp10
+# Refresh-Path
+# Install-Scoop
+# Install-Tools
+# Configure-Clink
+# Configure-Registry
+# Add-PowerToys-Keybindings
+# Install-DMZ-White
+# Prompt-Install-FilePilot
+# Add-Godot-Exe-Env-Variable
 
+Print-Stuff
 
 Write-Host "Setup successful!" -ForegroundColor Green
